de492330eafad8edb68da1774e7e6e4c10b2e956
Adrián Martínez Sequera <sequer@hotmail.es>
2013-04-22 10:19:02 +0200

    prueba

--- Buscaminas/Buscaminas/Form1.cs
+++ Buscaminas/Buscaminas/Form1.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Buscaminas
-{
+{//trdtyrdytrf
     public partial class Form1 : Form
     {
         //declaro el array de botones
